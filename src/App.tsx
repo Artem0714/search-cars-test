a6bd27ba95417cb84a270882e18f50d0ea6e0700
Artem0714 <arfed96@gmail.com>
2023-04-03 22:56:27 +0300

    build(map): attempt to display yandex map with marker in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useCar } from './hooks/cars';
 import './App.css';
 import CarCard from './components/CarCard';
 import { ErrorMessage } from './components/ErrorMessage';
 import SortSelect from './components/SortSelect';
+import './ymaps.d.ts';
 
 export function App() {
   const {cars, error, deleteCardCar, changeCardCar, sortCardCar} = useCar();
 
+  useEffect(() => {
+    ymaps.ready().then(() => {
+      new ymaps.Map("map", {
+        center: [55.753995, 37.614069],
+        zoom: 12,
+        controls: ['smallMapDefaultSet']
+      });
+      new ymaps.Object({
+        geometry: {
+          type: "Point",
+          coordinates: [55.753995, 37.614069]
+        }
+      });
+    })
+  }, [])
+
   return (
     <div className='container'>
       <SortSelect sortCardCar={sortCardCar}/>
-      {/* <select onChange={event => sortCardCar(event.target.value)}>
-        <option value='id'>Default</option>
-        <option value='name'>Name</option>
-        <option value='price'>Price</option>
-      </select> */}
       {error && <ErrorMessage />}
       {!error && cars.map((car =>
         <CarCard 
@@ -25,6 +37,7 @@ export function App() {
           onChangeToApp={changeCardCar}
         />
       ))}
+      <div id='map' className='map' style={{width: '600px', height: '400px'}}></div>
     </div>
   );
 }
