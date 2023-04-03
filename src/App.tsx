9c7abce013f53dcba7f0c6c918abb94d48ff3eb7
Artem0714 <arfed96@gmail.com>
2023-04-03 14:51:11 +0300

    refactor(hooks, App): completed the development of functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useCar } from './hooks/cars';
 import './App.css';
 import CarCard from './components/CarCard';
 import { ErrorMessage } from './components/ErrorMessage';
+
 export function App() {
-  const {cars, error, deleteCardCar, changeCardCar} = useCar();
+  const {cars, error, deleteCardCar, changeCardCar, sortCardCar} = useCar();
+
+  // const sortHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   sortCardCar(event.target.value);
+  // }
 
   return (
     <div className='container'>
+      <select onChange={event => sortCardCar(event.target.value)}>
+        <option value='id'>Default</option>
+        <option value='name'>Name</option>
+        <option value='price'>Price</option>
+      </select>
       {error && <ErrorMessage />}
       {!error && cars.map((car =>
         <CarCard 
--- src/hooks/cars.ts
+++ src/hooks/cars.ts
@@ -13,6 +13,22 @@ export function useCar () {
         setCars(cars.filter(item => item.id !== car.id))
     }
 
+    function sortCardCar(value: string) {
+        if (value === 'name') {
+            setCars([...cars.sort((a, b) => {
+                const carA = a.name.toUpperCase();
+                const carB = b.name.toUpperCase();
+                if (carA < carB) return -1;
+                if (carA > carB) return 1;
+                return 0
+            })]);
+        } else if (value === 'price') {
+            setCars([...cars.sort((a, b) => a.price - b.price)])
+        } else if (value === 'id') {
+            setCars([...cars.sort((a, b) => a.id - b.id)])
+        }
+    }
+
     const fetchCarCard = () => {
     return fetch('https://test.tspb.su/test-task/vehicles')
     .then(response => {
@@ -38,8 +54,8 @@ export function useCar () {
     }
 
     useEffect(() => {    
-    getCarCard()
+        getCarCard()
     }, [])
 
-    return {cars, error, deleteCardCar, changeCardCar};
+    return {cars, error, deleteCardCar, changeCardCar, sortCardCar};
 }
